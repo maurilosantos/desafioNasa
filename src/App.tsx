9d8e413bceaa836f63a7c8ad62e3aba763afeeab
VinnyPC <vinisilvax3@gmail.com>
2023-10-07 00:08:02 -0300

    feat: texto1 routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,25 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import texto1 from './components/texto1/texto1'
+import Texto1 from './components/texto1/texto1'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     
-      <p>
-        VAMOS COLOCAR OS COMPONENTES AQUI...
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path={"/"} element={<Texto1 />}></Route>
+          <Route path={"/"} element={<Texto2 />}></Route>
+          <Route path={"/"} element={<Texto3 />}></Route>
+          <Route path={"/"} element={<Texto4 />}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
 export default App
